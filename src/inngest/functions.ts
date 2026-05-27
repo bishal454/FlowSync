f6864486348a29d821ebf22e1f137a6a9f345e19
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2026-05-28 03:52:47 +0545

    add sentry  ai error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,6 +6,7 @@ import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
 
 import { generateText } from "ai";
+import * as Sentry from "@sentry/nextjs";
 import { setHeapSnapshotNearHeapLimit } from "node:v8";
 
 //it will create an instance of the Google Generative AI client that we can use to call the Google Generative AI API. So, we can use this client to generate text using the Google Generative AI API using generateText..
@@ -20,7 +21,12 @@ export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+    Sentry.setConversationId(event.id);
     await step.sleep("pretend", "5s");
+    Sentry.metrics.count("test_metric", 1);
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
     const { steps: geministeps } = await step.ai.wrap(
       "gemini-generate-text", // this is the name of the step that we can use to reference it later in the workflow.
       generateText, // it generate text which takes an object as an argument that contains the model, system, and prompt
@@ -28,6 +34,12 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.5-flash"), //model to use for generating text
         system: "You are a helpful assistant ", //instructions to the model
         prompt: "what is 2+2?", //input text that we want to generate a response for.
+        experimental_telemetry: {
+          isEnabled: true,
+          functionId: "gemini-generate-text",
+          recordInputs: true,
+          recordOutputs: true,
+        },
       },
     );
 
@@ -38,6 +50,12 @@ export const execute = inngest.createFunction(
         model: openai("gpt-4"), //model to use for generating text
         system: "You are a helpful assistant ", //instructions to the model
         prompt: "what is 2+2?", //input text that we want to generate a response for.
+        experimental_telemetry: {
+          isEnabled: true,
+          functionId: "openai-generate-text",
+          recordInputs: true,
+          recordOutputs: true,
+        },
       },
     );
 
@@ -48,6 +66,12 @@ export const execute = inngest.createFunction(
         model: anthropic("claude-sonnet-4-5"), //model to use for generating text
         system: "You are a helpful assistant ", //instructions to the model
         prompt: "what is 2+2?", //input text that we want to generate a response for.
+        experimental_telemetry: {
+          isEnabled: true,
+          functionId: "anthropic-generate-text",
+          recordInputs: true,
+          recordOutputs: true,
+        },
       },
     );
     return {
