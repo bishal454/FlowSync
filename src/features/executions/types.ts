d750273ee870da13c29f2b857bca67002c04b796
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2026-07-21 21:02:57 +0530

    add discord , slack node for  message usign web hook url

--- src/features/executions/types.ts
+++ src/features/executions/types.ts
@@ -1,4 +1,4 @@
-import  type { Realtime } from "@inngest/realtime";
+import type { Realtime } from "@inngest/realtime";
 import type { GetStepTools, Inngest } from "inngest";
 
 export type WorkflowContext = Record<string, unknown>;
@@ -8,9 +8,10 @@ export type StepTools = GetStepTools<Inngest.Any>;
 export interface NodeExecutorParams<TData = Record<string, unknown>> {
   data: TData;
   nodeId: string;
+  userId: string;
   context: WorkflowContext;
   step: StepTools;
-  publish:Realtime.PublishFn;
+  publish: Realtime.PublishFn;
 };
 
 export type NodeExecutor<TData = Record<string, unknown>> = (
