301cfbf1929c68463dd7cb1b1e42246137679452
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2026-07-22 01:02:51 +0530

    add github and google auth with better-auth

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -56,6 +56,35 @@ export function RegisterForm() {
     },
   });
 
+    const signInGithub = async () => {
+    await authClient.signIn.social({
+      provider: "github",
+    }, {
+      onSuccess: () => {
+        router.push("/");
+      },
+      onError: () => {
+        toast.error("Something went wrong");
+      },
+    });
+  };
+
+  const signInGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+    }, {
+      onSuccess: () => {
+        router.push("/");
+      },
+      onError: () => {
+        toast.error("Something went wrong");
+      },
+    });
+  };
+
+
+
+
   const onSubmit = async (values: RegisterFormValues) => {
     // Handle form submission, e.g., send data to the server
     await authClient.signUp.email(
@@ -96,6 +125,7 @@ export function RegisterForm() {
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
                   <Button
+                  onClick={signInGithub}
                     variant="outline"
                     className="w-full"
                     disabled={isPending}
@@ -110,6 +140,7 @@ export function RegisterForm() {
                     continue with Github
                   </Button>
                   <Button
+                  onClick={signInGoogle}
                     variant="outline"
                     className="w-full"
                     disabled={isPending}
